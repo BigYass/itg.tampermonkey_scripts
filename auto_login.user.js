6690db63e9ae032175bbbf3d329033d686f05ca0
yassine@itguard.fr <yassine@itguard.fr>
2025-07-15 10:50:53 +0200

    tt

--- auto_login.user.js
+++ auto_login.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AteraAutoLogin
 // @namespace    http://tampermonkey.net/
-// @version      1.0.5
+// @version      1.0.6
 // @description  Au login on Atera
 // @author       BigYass
 // @match        https://auth.atera.com/* 
