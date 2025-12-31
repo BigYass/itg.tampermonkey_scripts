8f6d8a3d804f2eb1b49b8583ebeffcb84ea20d10
BigYass <yassine.bamhaoud@outlook.com>
2025-12-31 14:38:21 +0100

    hohoho

---
+++ proxy_screen.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Screen Proxy
+// @namespace    http://tampermonkey.net/
+// @version      2025-11-17
+// @description  Un proxy pour ConnectWise
+// @author       BigYass
+// @match        https://connect.itguard.fr/*
+// @icon         https://connect.itguard.fr/FavIcon.axd
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+  
+
+
+})();
