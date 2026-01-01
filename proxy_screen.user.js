c7a54bb98a0a3fcf5b8783d2dddcf1524de0df24
BigYass <yassine.bamhaoud@outlook.com>
2026-01-01 10:01:33 +0100

    Added hooks

--- proxy_screen.user.js
+++ proxy_screen.user.js
@@ -14,6 +14,22 @@
 (function() {
   'use strict';
 
+  
+  /* -------- fetch -------- */
+  const _fetch = window.fetch;
+  window.fetch = function (...args) {
+    try {
+      const url = typeof args[0] === 'string' ? args[0] : args[0]?.url;
+      console.log('[fetch]', url);
+    } catch {}
+    return _fetch.apply(this, args);
+  };
 
+  /* -------- XMLHttpRequest -------- */
+  const _open = XMLHttpRequest.prototype.open;
+  XMLHttpRequest.prototype.open = function (method, url) {
+    console.log('[xhr]', method, url);
+    return _open.apply(this, arguments);
+  };
 
 })();
