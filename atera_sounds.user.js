950fef27b75ac7000beb506b05a977bb80bdaa38
BigYass <yassine.bamhaoud@outlook.com>
2026-01-02 17:06:24 +0100

    Compressed the 2 atera scripts

--- atera_sounds.user.js
+++ atera_sounds.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AteraSounds
 // @namespace    http://tampermonkey.net/
-// @version      1.0.3
+// @version      1.0.4
 // @description  Change Atera Alerts Sounds to customs one
 // @author       BigYass
 // @match        https://app.atera.com/*
@@ -10,52 +10,4 @@
 // @updateURL https://raw.githubusercontent.com/BigYass/itg.tampermonkey_scripts/main/atera_sounds.user.js
 // @downloadURL https://raw.githubusercontent.com/BigYass/itg.tampermonkey_scripts/main/atera_sounds.user.js
 // ==/UserScript==
-
-(function() {
-  'use strict';
-
-  // 1) Déclare un objet de mappage : url/original → url/ton-sound
-  const soundReplacements = {
-    'bike.mp3': null,
-    'door.mp3': 'none',
-    'horn.mp3': null,
-    'houston.mp3': 'https://github.com/BigYass/ressources/raw/refs/heads/main/ding.mp3',
-    'siren.mp3': null,
-  };
-
-  const old_play = HTMLAudioElement.prototype.play
-
-  HTMLAudioElement.prototype.play = function(...args) {
-    console.log('▶️ Interception d’un .play()');
-    try {
-      if (this.src) {
-        console.log('src = ', this.src)
-
-
-        for (const [key, value] of Object.entries(soundReplacements)) {
-          if (this.src.endsWith(key)) {
-            if (value) {
-              if (value === 'none') return Promise.resolve()
-
-              this.src = value
-              this.load()
-
-              console.log("new src =", this.src)
-            }
-          }
-
-        }
-      }
-    } catch (error) {
-      console.log('erreur : ', error)
-    }
-
-    return old_play.apply(this, args);
-  }
-
-  console.log('✅ Script de remplacement de sons chargé.');
-
-
-
-
-})();
+!function(){"use strict";const o={"bike.mp3":null,"door.mp3":"none","horn.mp3":null,"houston.mp3":"https://github.com/BigYass/ressources/raw/refs/heads/main/ding.mp3","siren.mp3":null},e=HTMLAudioElement.prototype.play;HTMLAudioElement.prototype.play=function(...s){console.log("▶️ Interception d’un .play()");try{if(this.src){console.log("src = ",this.src);for(const[e,s]of Object.entries(o))if(this.src.endsWith(e)&&s){if("none"===s)return Promise.resolve();this.src=s,this.load(),console.log("new src =",this.src)}}}catch(o){console.log("erreur : ",o)}return e.apply(this,s)},console.log("✅ Script de remplacement de sons chargé.")}();
--- auto_login.user.js
+++ auto_login.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AteraAutoLogin
 // @namespace    http://tampermonkey.net/
-// @version      1.0.9
+// @version      1.0.10
 // @description  Au login on Atera
 // @author       BigYass
 // @match        https://auth.atera.com/* 
@@ -11,93 +11,4 @@
 // @downloadURL https://raw.githubusercontent.com/BigYass/itg.tampermonkey_scripts/main/auto_login.user.js
 // @grant        none
 // ==/UserScript==
-
-(function() {
-  'use strict';
-  console.log("AteraAutoLogin starting")
-
-  const retry_interval = 200
-  const max_try = 120
-
-  const timeout = 0
-
-  let tries = 0
-
-  /**
-   * 
-   * @param {HTMLAllCollection} button The button that we want to click 
-   * @returns {boolean} if we click the button or not given the situation of the page and what button we want to click
-   */
-  const isClickAllowed = (button) => {
-    const queries = [
-      'input[name="username"]',
-      'input[name="password"]',
-      'input[name="code"]',
-    ]
-
-    for (const query of queries) {
-      const input = document.querySelector(query)
-      console.log('input = ' + input)
-
-      if (input && input.name && input.name === 'code') return true // If auth, then we do not click in any case
-
-      if (input && !input.value.trim()) {
-        console.log(query + " empty!")
-        return true
-      } else {
-        console.log(query + " not empty (or not found)")
-      }
-    }
-
-    return false
-  }
-
-  /**
-   * 
-   * @returns Find the first button that qualify as a button that we have to click
-   */
-  const getButton = () => {
-    const queries = [
-      'button[value="pick-authenticator"]',
-      'button[value="otp::0"]',
-      'button[value="default"]',
-      'a.link[_ngcontent-ng-c1690244427=""]', // <a _ngcontent-ng-c1690244427="" class="link">Back to login</a>
-      'a.link[_ngcontent-ng-c218642649=""]', // <a _ngcontent-ng-c218642649="" class="link">Back to login</a>
-    ]
-
-    for (const query of queries) {
-      const button = document.querySelector(query)
-      if (button) {
-        return button
-      }
-    };
-
-    return null
-  }
-
-  const loop = () => {
-    let again = true
-
-    tries += 1
-    console.log("loop, tries = " + tries)
-
-    const button = getButton()
-
-    if (button) {
-      console.log(button + "found!")
-
-      if (!isClickAllowed(button)) {
-        button.click()
-        again = false
-      }
-    } else {
-      console.log('Button not found...')
-    }
-
-    if (again && tries < max_try) setTimeout(loop, retry_interval)
-  }
-
-  // window.addEventListener('DOMContentLoaded', loop);
-
-  loop()
-})();
+!function(){"use strict";console.log("AteraAutoLogin starting");let o=0;const n=()=>{let t=!0;o+=1,console.log("loop, tries = "+o);const e=(()=>{const o=['button[value="pick-authenticator"]','button[value="otp::0"]','button[value="default"]','a.link[_ngcontent-ng-c1690244427=""]','a.link[_ngcontent-ng-c218642649=""]'];for(const n of o){const o=document.querySelector(n);if(o)return o}return null})();e?(console.log(e+"found!"),(()=>{const o=['input[name="username"]','input[name="password"]','input[name="code"]'];for(const n of o){const o=document.querySelector(n);if(console.log("input = "+o),o&&o.name&&"code"===o.name)return!0;if(o&&!o.value.trim())return console.log(n+" empty!"),!0;console.log(n+" not empty (or not found)")}return!1})()||(e.click(),t=!1)):console.log("Button not found..."),t&&o<120&&setTimeout(n,200)};n()}();
